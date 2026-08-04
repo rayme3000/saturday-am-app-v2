4ed90461486aec787fd8c5442ac052acfa0d9039
Raymond Brown <rebrown704@gmail.com>
2026-08-04 17:28:39 -0400

    fixed app icon

--- vite.config.ts
+++ vite.config.ts
@@ -7,34 +7,28 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
-      },
-      devOptions: {
-        enabled: true, // This allows you to test the PWA in 'npm run dev' too!
-      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa-192x192-v2.png', 'pwa-512x512-v2.png'],
       manifest: {
         name: 'Saturday AM',
-        short_name: 'Sat AM',
-        description: 'The premier manga reading app.',
+        short_name: 'Saturday AM',
+        description: 'The premier destination for diverse manga.',
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
-        start_url: '/',
+        orientation: 'portrait',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: 'pwa-192x192-v2.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: 'pwa-512x512-v2.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: 'pwa-512x512-v2.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
